018d5cdce87c176429c22d8562d8db78a3d81c72
Zouhair Mahieddine <zouhair.mahieddine@zalando.de>
2014-03-27 11:20:04 +0100

    v1.0.1 not using UIScrollView anymore (First Steps)

--- ZMScrollSprite/ZMScrollSprite.h
+++ ZMScrollSprite/ZMScrollSprite.h
@@ -1,6 +1,5 @@
 //
 //  ZMScrollSprite.h
-//  ZMScrollSpriteProject
 //
 //  Created by Zouhair Mahieddine on 3/17/14.
 //  Copyright (c) 2014 Zedenem. All rights reserved.
@@ -8,18 +7,31 @@
 
 #import <SpriteKit/SpriteKit.h>
 
+@class ZMScrollSprite;
+@protocol ZMScrollSpriteDelegate <NSObject>
+
+@optional
+- (void)scrollSpriteDidScroll:(ZMScrollSprite *)scrollSprite;
+
+@end
+
 @interface ZMScrollSprite : SKSpriteNode
 
 #pragma mark Initialization
 - (instancetype)initWithSize:(CGSize)size contentSize:(CGSize)contentSize;
 + (instancetype)scrollSpriteWithSize:(CGSize)size contentSize:(CGSize)contentSize;
 
-#pragma mark Scrolling
+#pragma mark Managing the Display of Content
 @property(assign, nonatomic) CGSize contentSize;
 @property(assign, nonatomic) CGPoint contentOffset;
 - (void)setContentOffset:(CGPoint)contentOffset animated:(BOOL)animated;
+@property(nonatomic, readonly) CGPoint minimumContentOffset;
+@property(nonatomic, readonly) CGPoint maximumContentOffset;
 
-#pragma mark UIScrollView Helpers
-- (void)sceneDidMoveToView:(UIView *)view;
+#pragma mark Managing Scrolling
+- (void)scrollRectToVisible:(CGRect)rect animated:(BOOL)animated;
+
+#pragma mark Managing the Delegate
+@property(weak, nonatomic) id<ZMScrollSpriteDelegate> delegate;
 
 @end
